0d34dd6a3db5297e3888c9deaae2ffe6c91310dc
Genevieve Schultz <genschultz@gmail.com>
2018-07-13 11:04:15 -0700

    added better password validation to Signup Component

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,60 +1,72 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
 class Signup extends Component {
-    constructor(props){
-      super(props)
-      this.state = {
-        name: '',
-        email: '',
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    }
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleNameChange(e) {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  handleEmailChange(e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  handlePasswordChange(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.password.length < 8 || this.state.password > 99) {
+      // Password does not meet length requirements
+      this.setState({
+        error: {
+          type: 'auth_error',
+          status: 401,
+          message: 'Password must be between 8 and 99 characters.'
+        },
         password: ''
-      }
-      this.handleNameChange = this.handleNameChange.bind(this);
-      this.handleEmailChange = this.handleEmailChange.bind(this);
-      this.handlePasswordChange = this.handlePasswordChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      }
+      })
+    } else {
+      axios.post('/auth/signup', {
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password
+      }).then(result => {
+        localStorage.setItem('mernToken', result.data.token)
+        this.props.liftToken(result.data)
+      }).catch(err => console.log(err))
+    }
+  }
 
-handleNameChange(e) {
-  this.setState({
-    name: e.target.value
-  })
-}
-
-handleEmailChange(e) {
-  this.setState ({
-    email: e.target.value
-  })
-}
-
-handlePasswordChange(e) {
-  this.setState ({
-    password: e.target.value
-  })
-}
-
-handleSubmit(e) {
-  e.preventDefault();
-  axios.post('/auth/signup', {
-    name: this.state.name,
-    email: this.state.email,
-    password: this.state.password
-  }).then( result => {
-    localStorage.setItem('mernToken', result.data.token)
-    this.props.liftToken(result.data)
-  }).catch( err => console.log(err) )
-}
-
-render() {
-  return (
-    <form onSubmit={this.handleSubmit}>
-      Name: <input type='text' value={this.state.name} onChange={this.handleNameChange} /><br />
-      Email: <input type='email' value={this.state.email} onChange={this.handleEmailChange} /><br />
-      Password: <input type='password' value={this.state.password} onChange={this.handlePasswordChange} /><br />
-      <input type="submit" value="SIGN UP" />
-    </form>
-  )
-}
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        Name: <input type='text' value={this.state.name} onChange={this.handleNameChange} /><br />
+        Email: <input type='email' value={this.state.email} onChange={this.handleEmailChange} /><br />
+        Password: <input type='password' value={this.state.password} onChange={this.handlePasswordChange} /><br />
+        <input type="submit" value="SIGN UP" />
+      </form>
+    )
+  }
 }
 
 export default Signup;
