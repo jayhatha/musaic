650395d68a93621a378fb6ff6f3f7722f3eb8a62
jayhatha <jay@jayhathaway.com>
2018-07-17 09:25:18 -0700

    working on spotify

--- client/src/Spotify.js
+++ client/src/Spotify.js
@@ -1,27 +1,54 @@
 import React, {Component} from 'react';
 
 class Spotify extends Component {
-  constructor(){
-  super();
-  const params = this.getHashParams();
-  console.log(params);
-}
-getHashParams() {
-  var hashParams = {};
-  var e, r = /([^&;=]+)=?([^&;]*)/g,
-      q = window.location.hash.substring(1);
-  e = r.exec(q)
-  while (e) {
-     hashParams[e[1]] = decodeURIComponent(e[2]);
-     e = r.exec(q);
-  }
-  return hashParams;
-}
+
+
+// OKAY SO HOW THIS SHOULD WORK IS:
+// User clicks 'log in with Spotify' - the login form is displayed
+
 render() {
   return (
-    <div className="App">
-      <a href='http://localhost:3000'> Login to Spotify </a>
+    // login part
+    <div class="container">
+      <div id="login">
+        <h1>This is an example of the Authorization Code flow</h1>
+        <a href="/login" class="btn btn-primary">Log in with Spotify</a>
+      </div>
+      <div id="loggedin">
+        <div id="user-profile">
+        </div>
+        <div id="oauth">
+        </div>
+        <button class="btn btn-default" id="obtain-new-token">Obtain new token using the refresh token</button>
+      </div>
+    // logged in part
+    <div>
+      <div class="media">
+        <div class="pull-left">
+          <img class="media-object" width="150" src="{{images.0.url}}" />
+        </div>
+        <div class="media-body">
+          <dl class="dl-horizontal">
+            <dt>Display name</dt><dd class="clearfix">{{display_name}}</dd>
+            <dt>Id</dt><dd>{{id}}</dd>
+            <dt>Email</dt><dd>{{email}}</dd>
+            <dt>Spotify URI</dt><dd><a href="VARIABLE NAMED EXTERNAL URLS DOT SPOTIFY">VARIABLE NAMED EXTERNAL URLS DOT SPOTIFY</a></dd>
+            <dt>Link</dt><dd><a href="{{href}}">{{href}}</a></dd>
+            <dt>Profile Image</dt><dd class="clearfix"><a href="{{IMAGES DOT ZERO DOT URL}}">IMAGES DOT ZERO DOT URL</a></dd>
+            <dt>Country</dt><dd>{{country}}</dd>
+          </dl>
+        </div>
+      </div>
     </div>
+    // oAuth part
+    <div>
+      <h2>oAuth info</h2>
+      <dl class="dl-horizontal">
+        <dt>Access token</dt><dd class="text-overflow">{{access_token}}</dd>
+        <dt>Refresh token</dt><dd class="text-overflow">{{refresh_token}}</dd>
+      </dl>
+    </div>
+  </div>
   );
 }
 }
